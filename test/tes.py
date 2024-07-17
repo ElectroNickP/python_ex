e49ae52b6bc594b156be5b542303f03c03f252e2
ElectroNick_P <nickpanko@mail.ru>
2024-07-17 19:03:46 +0700

    Update tes.py

--- test/tes.py
+++ test/tes.py
@@ -4,6 +4,7 @@ import allure
 @allure.label("issue", "N2-1")
 @allure.label("issue", "N2-2")
 @allure.label("issue", "N2-3")
+@allure.label("issue", "N2-4")
 @allure.epic("Web interface")
 @allure.epic("Web interface2")
 @allure.issue("N2-2")
